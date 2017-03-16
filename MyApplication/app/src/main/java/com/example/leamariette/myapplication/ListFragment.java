9e6e75d214e8e1f4ec9095afb02a3975fd0bc360
Alkalyne92 <lea.mrtte@gmail.com>
2017-03-16 10:50:04 +0100

    Super commiiiiiit

--- MyApplication/app/src/main/java/com/example/leamariette/myapplication/ListFragment.java
+++ MyApplication/app/src/main/java/com/example/leamariette/myapplication/ListFragment.java
@@ -18,49 +18,49 @@ import java.util.ArrayList;
 
 public class ListFragment extends Fragment implements ClickListener {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.list, container, false);
-    }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.list, container, false);
+        }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        RecyclerView rView = (RecyclerView) view.findViewById(android.R.id.list);
-        rView.setLayoutManager(new LinearLayoutManager(getContext()));
-        //rView.setAdapter(new MyAdapter(new String[]{"Coucou c'est moi Zelda", "Et moi 2", "Voiçi le 3em", "Le 4em", "Et le numéro complémentaire, le 5"}));
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            RecyclerView rView = (RecyclerView) view.findViewById(android.R.id.list);
+            rView.setLayoutManager(new LinearLayoutManager(getContext()));
+            //rView.setAdapter(new MyAdapter(new String[]{"Coucou c'est moi Zelda", "Et moi 2", "Voiçi le 3em", "Le 4em", "Et le numéro complémentaire, le 5"}));
 
-        // Créer une liste de News
-        ArrayList<News> newses = new ArrayList<>();
-        // Y ajouter des News (items seuls)
-        newses.add(new News("Super titre", "Super description", "Nouveautés", true));
-        newses.add(new News("Un autre titre absolument génial", "Much description", "Nouveautés", true));
-        newses.add(new News("Une dernière news, histoire de.", "What a description!", "Nouveautés", true));
-        newses.add(new News("The last ouane", "What a i dont care!", "pouet", true));
+            // Créer une liste de News
+            ArrayList<News> newses = new ArrayList<>();
+            // Y ajouter des News (items seuls)
+            newses.add(new News("Super titre", "Super description", "Nouveautés", true));
+            newses.add(new News("Un autre titre absolument génial", "Much description", "Nouveautés", true));
+            newses.add(new News("Une dernière news, histoire de.", "What a description!", "Nouveautés", true));
+            newses.add(new News("The last ouane", "What a i dont care!", "pouet", true));
 
-        rView.setAdapter(new MyAdapter(newses, this));
+            rView.setAdapter(new MyAdapter(newses, this));
 
-    }
+        }
 
-    @Override
-    // Quand l'HeaderItem est cliqué..
-    public void onHeaderItemClicked(News news) {
-        // Quand je clique, je créer une seconde activité.
-        Intent intent = new Intent(getActivity(), SecondActivity.class);
-        // Je lui envoi l'objets "One_News" qui contient les paramètres (news) de la News en cours.
-        intent.putExtra("OneNews", news);
-        // Je start la seconde activité. = intent
-        startActivity(intent);
-    }
+        @Override
+        // Quand l'HeaderItem est cliqué..
+        public void onHeaderItemClicked(News news) {
+            // Quand je clique, je créer une seconde activité.
+            Intent intent = new Intent(getActivity(), SecondActivity.class);
+            // Je lui envoi l'objets "One_News" qui contient les paramètres (news) de la News en cours.
+            intent.putExtra("OneNews", news);
+            // Je start la seconde activité. = intent
+            startActivity(intent);
+        }
 
-    @Override
-    public void onItemClicked(News news) {
-        // Quand je clique, je créer une seconde activité.
-        Intent intent = new Intent(getActivity(), SecondActivity.class);
-        // Je lui envoi l'objets "One_News" qui contient les paramètres (news) de la News en cours.
-        intent.putExtra("OneNews", news);
-        // Je start la seconde activité. = intent
-        startActivity(intent);
-    }
+        @Override
+        public void onItemClicked(News news) {
+            // Quand je clique, je créer une seconde activité.
+            Intent intent = new Intent(getActivity(), SecondActivity.class);
+            // Je lui envoi l'objets "One_News" qui contient les paramètres (news) de la News en cours.
+            intent.putExtra("OneNews", news);
+            // Je start la seconde activité. = intent
+            startActivity(intent);
+        }
 
 
 
--- MyApplication/app/src/main/java/com/example/leamariette/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/leamariette/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.leamariette.myapplication;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity  {
@@ -10,9 +14,29 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, new ListFragment())
-                .commit();
+        ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+    }
+
+
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return 5;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return new ListFragment();
+        }
+
+        @Override
+        public String getPageTitle(int position) {
+            return "Cochon Pôl";
+        }
     }
 
 }
